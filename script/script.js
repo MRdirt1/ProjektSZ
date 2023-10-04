8b5c950d7b23fcf8abf312622a8cc769bc146f51
MRdirt1 <144840872+MRdirt1@users.noreply.github.com>
2023-10-04 17:15:16 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,13 +1,13 @@
-javascript
+
 window.onload = function() {
- showPopup();
+    showPopup();
 };
 
 function showPopup() {
- var popup = document.getElementById("popup");
- popup.style.display = popup.style.display === "none" ? "block" : "none";
+    var popup = document.getElementById("popup");
+    popup.style.display = popup.style.display === "none" ? "block" : "none";
 }
 
 function hidePopup() {
- showPopup();
+    showPopup();
 }
